d86c5e93b623eb2cdbabbc921cd5a49ea5b462a7
benedikturcan <benedikt_urcan@hotmail.de>
2023-02-26 17:02:30 +0100

    added subtract, divide and multiply fn

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -20,10 +20,38 @@ function getUserInput() {
   return parseInt(userInput.value);
 }
 
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+
 function add() {
   const enteredNum = getUserInput();
-  const description = `${currentResult} + ${enteredNum}`;
+  const initialResult = currentResult;
   currentResult = currentResult + enteredNum;
-  outputResult(currentResult, description);
+  createAndWriteOutput("+", initialResult, enteredNum);
 }
+
+function subtract() {
+  const enteredNum = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNum;
+  createAndWriteOutput("-", initialResult, enteredNum);
+}
+function multiply() {
+  const enteredNum = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNum;
+  createAndWriteOutput("*", initialResult, enteredNum);
+}
+function divide() {
+  const enteredNum = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNum;
+  createAndWriteOutput("/", initialResult, enteredNum);
+}
+
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
